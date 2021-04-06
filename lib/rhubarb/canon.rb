75996f04b1f081151f01421690e6d22e3196baaf
folkengine <gaoler@electronicpanopticon.com>
2021-04-05 20:04:45 -0400

    Added cyrillic ge

--- lib/rhubarb/canon.rb
+++ lib/rhubarb/canon.rb
@@ -3,7 +3,7 @@ module Rhubarb
   class Canon
     attr_reader :orator
 
-    BASE_URL = "http://127.0.0.1:3000/greetings/howdy?name="
+    BASE_URL = "http://127.0.0.1:3000/greetings/howdy?name=".freeze
 
     def initialize
       @orator = Orator.new
@@ -20,15 +20,16 @@ module Rhubarb
 
     def initialize_recorder
       time = Time.now.utc.iso8601
-
     end
 
     def fire(name = RandomNameGenerator.flip_mode.compose)
-      url = "#{BASE_URL}#{name}"
+      escaped_name = CGI.escape(name)
+      url = "#{BASE_URL}#{escaped_name}"
 
       begin
         HTTParty.get(url)
         name(name)
+        @logger.info name
         true
       rescue Errno::ECONNREFUSED
         @orator.alert("Connection refused")
@@ -37,7 +38,6 @@ module Rhubarb
     end
 
     def name(name = RandomNameGenerator.flip_mode.compose)
-      @logger.error name
       @orator.speech name
       name
     end
