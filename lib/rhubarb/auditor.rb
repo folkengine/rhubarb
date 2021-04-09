29918dcb09a3d02f8203ca8fc9d937266b5af758
christopher.r.baker <79522066+christophbaker@users.noreply.github.com>
2021-04-09 12:20:08 -0400

    updated connection string

--- lib/rhubarb/auditor.rb
+++ lib/rhubarb/auditor.rb
@@ -4,12 +4,14 @@ module Rhubarb
   class Auditor
     attr_reader :connection
 
+    DBCONN = "postgres://sjljbqoxxlkvuv:481af6b5854859dfee8fcbf9ee213c69ebda6f90bd4ba408b2fd68e41a91f4c7@ec2-3-211-37-117.compute-1.amazonaws.com:5432/dbufrefjald3e2"
+
     def initialize
-      @connection = PG::Connection.new(ENV["DATABASE_URL"])
+      @connection = PG::Connection.new(DBCONN)
     end
 
     def contains?(name)
-      res @connection.exec_params("SELECT * where name = $1", [name])
+      res @connection.exec_params("SELECT * from names where name = $1", [name])
       res.present?
     end
   end
